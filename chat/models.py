b4e99f976624ab9e1d6427c63bc0eaf8c3959b45
Michael Tingley <michaelalantingley@gmail.com>
2016-01-19 18:09:27 -0800

    Remove unused assets, and beginning documentation

--- chat/models.py
+++ chat/models.py
@@ -1,9 +1,10 @@
+'''Database models for the chat application'''
+
 from datetime import datetime
 
 from django.contrib.auth.models import User
 from django.db import models
 
-from uuid import uuid4
 
 class Participant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
